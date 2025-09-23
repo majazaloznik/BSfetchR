5cceb2fc8bb2cde6809d97e3835255178a49cbbf
majazaloznik <maja.zaloznik@gmail.com>
2025-09-23 17:31:09 +0200

    more test debugging

--- tests/testthat/test-importing.R
+++ tests/testthat/test-importing.R
@@ -1,3 +1,21 @@
+test_that("Debug SQL differences", {
+  with_mock_db({
+    con <- make_test_connection()
+
+    # Capture what SQL is actually being generated
+    dittodb::start_db_capturing()
+
+    # This will fail but show us the actual SQL being generated
+    tryCatch({
+      result <- BS_import_structure("F2_Q1S", con, schema = "platform")
+    }, error = function(e) {
+      message("Error: ", e$message)
+    })
+
+    dittodb::stop_db_capturing()
+  })
+})
+
 test_that("BS_import_structure works correctly with mocked dimension_selector", {
   with_mock_db({
     con <- make_test_connection()
