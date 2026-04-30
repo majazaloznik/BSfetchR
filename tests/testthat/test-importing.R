1e16806e8643a01663297265c4af6815fe21552a
majazaloznik <maja.zaloznik@gmail.com>
2026-04-30 10:12:30 +0200

    Fix failing gha test with timezone setting. new fixtures obvs

--- tests/testthat/test-importing.R
+++ tests/testthat/test-importing.R
@@ -9,15 +9,15 @@ test_that("BS_import_structure works correctly with mocked dimension_selector", 
 })
 
 test_that("BS_import_data works correctly", {
-  options(dittodb.debug = TRUE)
-  with_mock_db({
-    con <- make_test_connection()
-    result <- BS_import_data_points("I2_4_4AS", con, schema = "platform")
-    expect_true(all(names(result) == c("vintages", "data")))
-    expect_true(all(names(result$data) == c("periods_inserted", "datapoints_inserted",
-                                            "flags_inserted")))
-    print(result$data$datapoints_inserted)
-    expect_true(result$data$datapoints_inserted == 28356)
+  withr::with_timezone("UTC", {
+    with_mock_db({
+      con <- make_test_connection()
+      result <- BS_import_data_points("I2_4_4AS", con, schema = "platform")
+      expect_true(all(names(result) == c("vintages", "data")))
+      expect_true(all(names(result$data) == c("periods_inserted", "datapoints_inserted",
+                                              "flags_inserted")))
+      print(result$data$datapoints_inserted)
+      expect_true(result$data$datapoints_inserted == 28356)
+    })
   })
-  options(dittodb.debug = FALSE)
 })
